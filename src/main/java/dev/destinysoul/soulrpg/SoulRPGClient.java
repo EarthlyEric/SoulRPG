372e3856267d1952b1733fbd1db0f01dc66df46c
EarthlyEric <minecarft.creeper.eric@gmail.com>
2023-05-29 13:10:22 +0800

    init commit

---
+++ src/main/java/dev/destinysoul/soulrpg/SoulRPGClient.java
@@ -0,0 +1,10 @@
+package dev.destinysoul.soulrpg;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SoulRPGClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/dev/destinysoul/soulrpg/SoulRPGDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.destinysoul.soulrpg;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SoulRPGDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
